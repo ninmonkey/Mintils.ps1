d477161cdd4e8300d5f751fceafd8acef882469b
Jake Bolton <ninmonkeys@gmail.com>
2025-11-22 13:55:19 -0600

    add: command: `Find-MintilsTypeName`, `Get-MintilsTypeHelp`

---
+++ Commands/Find-MintilsTypeName.ps1
@@ -0,0 +1,45 @@
+﻿# requires -Module ClassExplorer
+
+function Find-MintilsTypeName {
+    <#
+    .SYNOPSIS
+        Get [Type] info from the names of types
+    .NOTES
+    .link
+        Mintils\Find-MintilsTypeName
+    .link
+        Mintils\Get-MintilsTypeHelp
+    #>
+    [CmdletBinding()]
+    [Alias('Mint.Find-TypeName')]
+    [OutputType( 'System.Reflection.TypeInfo', 'System.RuntimeType' )]
+    param(
+        # ex: 'System.TimeZoneInfo'
+        [Alias('FromName')]
+        [Parameter(Mandatory, ValueFromPipeline )]
+            [string] $TypeName,
+
+        # Ignore exceptions thrown by [Type]
+            [switch] $WithoutError
+    )
+    process {
+
+        $maybeType = [Type]::GetType( $TypeName,
+            <# bool: throwOnError #> (! $WithoutError ),
+            <# bool: ignoreCase #>   $true )
+
+        if( -not $MaybeType ) { # Fallback: exact FullName
+            $maybeType = Find-Type -fullname 'TimeZoneInfo' # ex: 'System.TimeZoneInfo'
+        }
+        if( -not $MaybeType ) { # Fallback: exact Name
+            $maybeType = Find-Type -Name $TypeName  # ex: 'TimeZoneInfo'
+        }
+        if( -not $MaybeType ) { # Fallback: Super wild card
+            $maybeType = Find-Type -Fullname "*${TypeName}*"
+        }
+        if( $MaybeType.count -gt 1 ) {
+            'Multiple types found for name: "{0}"' -f $TypeName | Write-Warning
+        }
+        $maybeType
+    }
+}
---
+++ Commands/Get-MintilsTypeHelp.ps1
@@ -0,0 +1,72 @@
+﻿
+
+function Get-MintilsTypeHelp {
+    <#
+    .SYNOPSIS
+        Find dotnet docs for a type
+    .NOTES
+    .link
+        Mintils\Find-MintilsTypeName
+    .link
+        Mintils\Get-MintilsTypeHelp
+    #>
+    [Alias('Mint.Get-TypeHelp')]
+    [Cmdletbinding( DefaultParameterSetName='FromString' )]
+    param(
+        # [Alias('InObj')]
+        # [Parameter( Mandatory, ParameterSetName='FromObject', Position = 0, ValueFromPipeline )]
+        # [object] $FromObject,
+        # # [type] $FromTypeData
+
+        [Parameter( Mandatory, ParameterSetName='FromString', Position = 0, ValueFromPipeline )]
+        [Alias('Name')]
+        [string] $TypeName,
+
+        # Open url
+        [Alias('Online')]
+        [switch] $Open
+    )
+
+    begin {
+        $PSCmdlet.ParameterSetName | Join-String -f 'using ParamSet: {0}' | Write-Verbose
+
+        function _UrlFromName {
+            param( [string] $Name )
+
+
+            [uri] $url = 'https://learn.microsoft.com/en-us/dotnet/api/{0}?view=net-10.0' -f $Name
+
+            return $Url
+        }
+    }
+    process {
+
+        [string] $shortName = ''
+        switch( $PSCmdlet.ParameterSetName ) {
+            # 'FromObject' {
+            #     # $type = $FromObject.GetType()
+            #     break
+            # }
+            'FromString' {
+                $shortName = $TypeName
+                break
+            }
+            default {
+                throw "Unhandled ParameterSet: $( $PSCmdlet.ParameterSetName )"
+            }
+        }
+
+        $shortName | Join-String -op 'Shortname: ' | Write-Verbose
+
+        $url  = _urlFromName -Name $shortName
+        $info = [pscustomobject]@{
+            PSTypeName = 'Mintils.TypeName.Url'
+            Type       = $shortName
+            Url        = $Url
+        }
+        if( $Open ) {
+            Start-Process -FilePath ( $Info.Url.ToString() )
+        }
+        return $Info
+    }
+}
