54edf40e4674d2e2806c7167d71529f3d8e98922
Jake Bolton <ninmonkeys@gmail.com>
2025-11-26 16:43:06 -0600

    add: command: `Format-RelativePath`

---
+++ Commands/Format-MintilsRelativePath.ps1
@@ -0,0 +1,45 @@
+﻿function Format-MintilsRelativePath {
+    <#
+    .synopsis
+        Sugar that converts paths relative a base dir
+    #>
+    [Alias('Mint.Format-RelativePath')]
+    # [OutputType( [string], 'Mintils.RelativePath' )]
+    [CmdletBinding()]
+    param(
+        [Alias('BasePath')]
+        [Parameter(Mandatory, Position = 0)]
+        $RelativeTo,
+
+        # Strings / paths to convert
+        [Alias('PSPath', 'FullName', 'InObj')]
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName, ValueFromPipeline)]
+        [string[]] $Path,
+
+        # Emit an object with path properties, including the raw original path
+        [Alias('PassThru')]
+        [switch] $AsObject
+    )
+    process {
+        foreach( $item in $Path ) {
+            $relPath = [System.IO.Path]::GetRelativePath(
+                <# string: relativeTo #> $RelativeTo,
+                <# string: path  #>  $Item )
+
+            if( -not $AsObject ) {
+                $relPath
+                continue
+            } else {
+                [pscustomobject]@{
+                    PSTypeName = 'Mintils.RelativePath'
+                    Path       = $relPath
+                    Original   = $Item
+                    RelativeTo = $RelativeTo
+                }
+                continue
+            }
+
+        }
+    }
+
+}
