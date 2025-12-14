6c180c2aca4d35bb3bed8969400895bd17202e41
Jake Bolton <ninmonkeys@gmail.com>
2025-12-14 14:10:55 -0600

    docs: `Select-Some` examples using `One` / `Some`

--- Commands/Select-MintilsObject.ps1
+++ Commands/Select-MintilsObject.ps1
@@ -2,13 +2,37 @@
     <#
     .synopsis
         Select first, last, some, etc...
+    .EXAMPLE
+        # when you want a few items
+        > Get-Module | Mint.One            # first only
+        > Get-Module | Mint.Select-Some    # up to 5
+        > Get-Module | Mint.Select-Some 20 # up to 20
+    .example
+        > 'a'..'f' | Mint.Select-Random -Shuffle | Mint.One
+    .example
+        # optional: enable aggressive aliases
+        > Mint.Enable-DefaultAlias
+
+        # like
+        > Get-Process | One
+        > Get-Process | Some # returns 5
+
+        > Get-Module | One
+    .link
+        Mintils\Select-MintilsRandomObject
+    .link
+        Mintils\Mint.Some
+    .link
+        Mintils\Mint.One
+    .link
+        Mintils\Mint.Select-Random
     #>
     [Alias('Mint.Select-Some', 'Mint.One', 'Mint.First' )]
     # [OutputType( [string], 'Mintils.RelativePath' )]
     [CmdletBinding()]
     param(
         # future: steppable pipeline for speed
-        [Alias('PSPath', 'FullName', 'InObj')]
+        [Alias('InObj', 'Obj')]
         [Parameter(Mandatory, ValueFromPipelineByPropertyName, ValueFromPipeline)]
         [object[]] $InputObject,
 
