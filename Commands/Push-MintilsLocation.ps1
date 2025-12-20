0d12f57680a1cc69895c22f645fef26750087810
Jake Bolton <ninmonkeys@gmail.com>
2025-12-20 13:48:50 -0600

    clean: refactor command `Mint.Push-Location` logic
    Supports types: ApplicationInfo, DirectoryInfo, FileSystemInfo, FunctionInfo, PSModuleInfo, String

--- Commands/Push-MintilsLocation.ps1
+++ Commands/Push-MintilsLocation.ps1
@@ -1,4 +1,59 @@
-﻿function Push-MintilsLocation {
+﻿function _Resolve-DirectoryFromPathlike {
+    <#
+    .SYNOPSIS
+        [private] get filepath if possible from many object types. Prefer filename with path, fallback to directory if needed.
+    .DESCRIPTION
+        returns $null if all paths failed, and writes a warning.
+    .example
+        # Externally test using this:
+        & ( ipmo Mintils -PassThru ) { _Resolve-DirectoryFromPathLike ( gcm Add-ExcelName ) -Debug }
+    #>
+    [CmdletBinding()]
+    param(
+        # File, Directory, PSModuleInfo, String, Etc
+        # Supports Types: ApplicationInfo, DirectoryInfo, FileSystemInfo, FunctionInfo, PSModuleInfo, String,
+        [object] $InputObject
+    )
+
+    $ResolvedItem = Get-Item -ea ignore $InputObject
+
+    [PSCustomObject]@{
+        ResolvedType    = ( $ResolvedPath )?.GetType().FullName
+        Resolved        = ( $ResolvedPath )?.ToString()
+        InputObjectType = ( $InputObject )?.GetType().FullName
+        InputObject     = ( $InputObject )?.ToString()
+    } | ConvertTo-Json -Depth 0 | Join-String -f 'Mint.Goto parameters: {0}' | Write-Debug
+
+    if( $InputObject -is [IO.FileSystemInfo] ) {
+        return $InputObject
+    }
+    if( $InputObject -is [System.Management.Automation.PSModuleInfo] ) {
+        return ( Get-Item $InputObject.Path )
+    }
+    if( $InputObject -is [IO.DirectoryInfo] ) {
+        return $InputObject
+    }
+    if( $InputObject -is [Management.Automation.FunctionInfo] ) {
+        [Management.Automation.FunctionInfo] $maybeFunc = $InputObject
+        $Resolved = Get-Item $maybeFunc.ScriptBlock.Ast.Extent.File
+        if( $null -eq $Resolved ) {
+            throw "Found [FunctionInfo] but Ast.Extent.File was null. ( Verify the module '$( $InputObject.Source  )' is loaded )"
+        }
+        return $Resolved
+    }
+    if( $InputObject -is [Management.Automation.ApplicationInfo] ) {
+        [Management.Automation.ApplicationInfo] $maybeApp = $InputObject
+        $Resolved = ( Get-Item $maybeApp.Source ) ?? ( Get-Item $maybeApp.Path )
+        return $Resolved
+    }
+    if( $InputObject -is [string] -and $Null -ne $ResolvedItem ) {
+        return $ResolvedItem
+    }
+    "Unhandled input type when converting '${InputObject}' of type $( ( $InputObject)?.GetType() ) to path: {0}" | Write-Warning
+    # Maybe always attempt PSPath ? Or leave that to the caller?
+}
+
+function Push-MintilsLocation {
     <#
     .synopsis
         go to a path, auto convert-paths, since push/pop doesn't 2025-10-29
@@ -25,47 +80,35 @@
         [object] $Directory,
 
         # Also output the directory you moved to as an object
-        [switch] $PassThru
+        [switch] $PassThru,
 
+        # writes new path to the console as dim text
+        [switch] $PSHost
         # [string] $StackName = 'mintils.goto' # might not affect user scope
     )
     begin {}
     end {
-        $ResolvedPath = Get-Item -ea ignore $InputObject
-        $Return = $null
+        $Resolved = _Resolve-DirectoryFromPathlike $InputObject
+        Join-String -f 'Resolved: "{0}"' -InputObject ( $Resolved )?.ToString() | Write-Verbose
 
-        [PSCustomObject]@{
-            ResolvedType           = ( $ResolvedPath )?.GetType().FullName
-            Resolved               = ( $ResolvedPath )?.ToString()
-            ParamObjectType    = ( $InputObject )?.GetType().FullName
-            ParamObject        = ( $InputObject )?.ToString()
-            ParamDirectoryType = ( $Directory )?.GetType().FullName
-            ParamDirectory     = ( $Directory )?.ToString()
-        } | ConvertTo-Json -Depth 0 | Join-String -f 'Mint.Goto parameters: {0}' | Write-Debug
-
-        switch( $ResolvedPath ) { # refactor as private
-            { $_ -is [IO.DirectoryInfo] } {
-                $null = Microsoft.PowerShell.Management\Push-Location -Path $ResolvedPath # -StackName $StackName
-                $Return = $ResolvedPath
-            }
-            { $_ -is [IO.FileSystemInfo] } {
-                Microsoft.PowerShell.Management\Push-Location -Path $ResolvedPath.Directory # -StackName $StackName
-                $Return = $ResolvedPath.Directory
-            }
-            default {
-                'Unhandled input type when converting to path: {0}' -f $InputObject.GetType().Name | Write-Warning
-            }
+        if( $Null -eq $Resolved ){
+            $Resolved = _Resolve-DirectoryFromPathlike $Directory
+        }
+        if( $Null -eq $Resolved ) {
+            throw "Unhandled error resolving '${Resolved}' of type: '$( ( $Resolved)?.GetType() )' "
         }
 
-        if( $PassThru ) { $return }
+        if( Test-Path -ea 'ignore' $Resolved.Directory ) {
+            $null = Microsoft.PowerShell.Management\Push-Location -Path $Resolved.Directory #
+        } elseif ( Test-Path -ea 'ignore' $Resolved ) {
+            $null = Microsoft.PowerShell.Management\Push-Location -Path $Resolved
+        }
+        if( $PSHost ) {
+            $Resolved | Join-String -f '    Move to => "{0}"'
+                | Write-Host -bg 'gray20' -fg 'gray30'
+        }
 
-
-        <#
-        other properties that include full filenames in path
-            [PSModuleInfo].Path
-
-            [ApplicationInfo].Source
-
-        #>
+        # $null = Microsoft.PowerShell.Management\Push-Location -Path $Resolved.Directory # -StackName 'mintils.goto'
+        if( $PassThru ) { return $Resolved } # emit objects instead of 'Push-Location'. Allowing found filename to be returned.
     }
 }
