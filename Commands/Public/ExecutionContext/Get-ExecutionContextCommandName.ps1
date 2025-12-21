c0bee7e6b22beabf04c70c12b5d5082b860a093a
Jake Bolton <ninmonkeys@gmail.com>
2025-12-21 10:00:57 -0600

    Command `ExecutionContext` for `GetCommandName` Fixes #89

---
+++ Commands/Public/ExecutionContext/Get-ExecutionContextCommandName.ps1
@@ -0,0 +1,32 @@
+﻿function Get-MintilsExecutionContextCommandName {
+    <#
+    .SYNOPSIS
+        sugar that wraps "$ExecutionContext.InvokeCommand.GetCommandName"
+    .notes
+        Maybe allow returning (Get-Item) as an option
+    .link
+        System.Management.Automation.CommandInvocationIntrinsics
+    #>
+    [Alias(
+        'Mint.ExecutionContext.Get-CommandName',
+        'Mint.ExContext.Get-CommandName'
+        # 'Mint.Get-ExContextCommandName'
+    )]
+    [CmdletBinding()]
+    # [OutputType( [Collections.Generic.List[String]] )]
+    param(
+        [Parameter(mandatory, ValueFromPipeline)]
+        [string] $Name,
+        [bool] $NameIsPattern,
+
+        # output filepath as name only
+        [switch] $AsText
+    )
+    process {
+        $query = $ExecutionContext.InvokeCommand.
+            GetCommandName( $Name, $NameIsPattern, $true )
+        if( $asText ) { return $query }
+
+        Get-Command ( $Query | Get-Item )
+    }
+}
