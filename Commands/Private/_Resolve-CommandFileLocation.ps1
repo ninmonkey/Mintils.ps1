7e399e1e4300bdc181775d1001d08c8fe228b91b
Jake Bolton <ninmonkeys@gmail.com>
2025-12-24 14:57:34 -0600

    clean: refactor resolving CommandInfo logic for `Mint.Find-FunctionDefinition`

---
+++ Commands/Private/_Resolve-CommandFileLocation.ps1
@@ -0,0 +1,136 @@
+﻿function _Resolve-CommandFileLocation {
+    <#
+    .SYNOPSIS
+        resolve many kinds into Filename and line numbers, if possible. [private/internal]
+    .NOTES
+        Some related types:
+        > Find-Type -namespace System.Management.Automation -Name '*Info'
+
+    expected 'Get-Command' output types:
+
+        (gcm gcm).OutputType | % Type | Join-String -p { $_.ToString() } -sep "`n" -f '[{0}]' | Sort-Object -Unique
+
+        [System.Management.Automation.AliasInfo]
+        [System.Management.Automation.ApplicationInfo]
+        [System.Management.Automation.FunctionInfo]
+        [System.Management.Automation.CmdletInfo]
+        [System.Management.Automation.ExternalScriptInfo]
+        [System.Management.Automation.FilterInfo]
+        [System.String]
+        [System.Management.Automation.PSObject]
+    .example
+        # Externally test using this:
+        & ( ipmo Mintils -PassThru ) { _Resolve-DirectoryFromPathLike ( gcm Add-ExcelName ) -Debug }
+    #>
+    [CmdletBinding()]
+    param(
+        # File, Directory, PSModuleInfo, String, Etc
+        # Supports Types like: AliasInfo, ApplicationInfo, CmdletInfo, DirectoryInfo, ExternalScriptInfo, FileSystemInfo, FunctionInfo, PSModuleInfo, FilterInfo, String, etc...
+        [Alias('Object', 'InObj')]
+        [ValidateNotNull()]
+        [Parameter(Mandatory)]
+        $InputObject
+    )
+    $InputObject.GetType()
+        | Join-String -f 'enter => type: {0} ' | Write-Verbose
+
+    $return = [ordered]@{
+        PSTypeName = 'mintils.Resolved.Command.OtherInfo'
+    }
+    switch( $InputObject ) {
+        { $_ -is [System.Management.Automation.AliasInfo] } {
+            $return.PSTypeName = 'Mintils.Resolved.Command.AliasInfo'
+            [Management.Automation.CommandInfo] $ResolvedCmd = $InputObject.ResolvedCommand
+
+            $return  = _Resolve-CommandFileLocation -Inp $ResolvedCmd
+            if( -not $return ) {
+                throw "nyi $( $InputObject.GetType() ) "
+            }
+            break
+        }
+        { $_ -is [System.Management.Automation.ApplicationInfo] } {
+            $return.PSTypeName = 'Mintils.Resolved.Command.ApplicationInfo'
+            throw "nyi $( $InputObject.GetType() ) "
+            break
+        }
+        { $_ -is [System.Management.Automation.FunctionInfo] } {
+            [System.Management.Automation.FunctionInfo] $func = $_
+
+            $return.PSTypeName = 'Mintils.Resolved.Command.FunctionInfo'
+            $return.Name       = $func.Name
+            $return.ModuleName = $func.ModuleName
+            $return.Module     = $func.Module
+            $return.Source     = $func.Source
+            $return.FileExists = $false                                                   # ensures order before File
+            $return.File       = Get-Item -ea 'ignore' $func.ScriptBlock.Ast.Extent.File
+            $return.FileExists = $return.File -and (  Test-Path $return.file )
+            # if (-not $return.File ) {
+            #     'bad?' | Write-Host -fg purple
+            #     $null = 0
+            # }
+            $return.StartLineNumber      = $func.ScriptBlock.Ast.Extent.StartLineNumber
+            $return.EndLineNumber        = $func.ScriptBlock.Ast.Extent.EndLineNumber
+            $return.StartColumnNumber    = $func.ScriptBlock.Ast.Extent.StartColumnNumber
+            $return.EndColumnNumber      = $func.ScriptBlock.Ast.Extent.EndColumnNumber
+            $return.FunctionInfoInstance = $func
+
+            if( -not $return.FileExists ) {
+                $return.FileWithLineNumberString = '' # fix: convert to calculated property
+            } else {
+                $return.FileWithLineNumberString = '{0}:{1}:{2}' -f @(
+                    $return.File.FullName,
+                    $return.StartLineNumber
+                    $return.StartColumnNumber
+                )
+            }
+            $return.HelpFile = $func.HelpFile
+            $return.HelpUri  = $func.HelpUri
+            # $return = 'func'
+            break
+        }
+
+        { $_ -is [System.Management.Automation.CmdletInfo] } {
+            [System.Management.Automation.CmdletInfo] $Info = $InputObject
+
+            $return.PSTypeName         = 'Mintils.Resolved.Command.CmdletInfo'
+            $return.Name               = $Info.Name
+            $return.ImplementingType   = $Info.ImplementingType
+            $return.Namespace          = $Info.Namespace
+            $return.Source             = $Info.Source
+            $return.DLL                = $Info.DLL
+            $return.FileExists = $true # ensure order
+            $return.File               = Get-Item $info.DLL
+            $return.FileExists = Test-Path -ea ignore $return.File
+            $return.CmdletInfoInstance = $Info
+            # throw "nyi $( $InputObject.GetType() ) "
+            break
+        }
+        { $_ -is [System.Management.Automation.ExternalScriptInfo] } {
+            # $return = ''
+            $return.PSTypeName = 'Mintils.Resolved.Command.ExternalScriptInfo'
+            throw "nyi $( $InputObject.GetType() ) "
+            break
+        }
+        { $_ -is [System.Management.Automation.FilterInfo] } {
+            # $return = ''
+            $return.PSTypeName = 'Mintils.Resolved.Command.FilterInfo'
+            throw "nyi $( $InputObject.GetType() ) "
+            break
+        }
+        {
+            $_ -is [String] -and ( $nextGcm = Gcm $_ -ea ignore)
+        } {
+            # $return        = 'StrToGcm => {0} ' -f $nextGcm
+            $return  = _Resolve-CommandFileLocation -Inp $nextGcm
+            # $return.PSTypeNames -join ', '
+            throw "nyi $( $InputObject.GetType() ) "
+            # $return += ' , then => {0}' -f (  $nextResolved )
+            break
+        }
+        default {
+            throw ("Unhandled type: $( $InputObject.GetType() )")
+            break
+        }
+    }
+    [pscustomobject] $return
+}
--- Commands/Public/Find-MintilsFunctionDefinition.ps1
+++ Commands/Public/Find-MintilsFunctionDefinition.ps1
@@ -1,5 +1,5 @@
 ﻿function Find-MintilsFunctionDefinition {
-    <#
+    <#verbo
     .synopsis
         Find a function, then open vscode to that exact line number
     .NOTES
@@ -39,46 +39,101 @@
                 [Parameter(Mandatory)] $Obj
             )
             if( $Obj -is [Management.Automation.AliasInfo] )  {
-                $resolve = Get-Command $Obj.Definition
+                [Management.Automation.AliasInfo] $Cur = $Obj
+
+                if( $Cur.ResolvedCommand ) {
+                    $Cur.ResolvedCommand.Name
+                        '    => resolved: "{0}" -> {1}' -f @(
+                            $obj.Name,
+                            $Obj.ResolvedCommandName
+                        )
+                        | Write-Verbose
+                    return ( CoerceCommand -Obj $Cur.ResolvedCommand )
+                } else {
+                    '    => "{0}.ResolvedCommand" was falsy ' -f $Obj.Name
+                        | Write-Verbose
+                }
+                # CoerceCommand -Obj $Obj.ReferencedCommand
+
+                # $Resolve = Get-Command $Cur.Definition
+                # [System.Management.Automation.CommandInfo] $
+
                 $msg = '    => start: {0} of "{1}"' -f @(
-                    $resolve.ScriptBlock.Ast.Extent.StartLineNumber
-                    $resolve.ScriptBlock.File
+                    $Resolve.ScriptBlock.Ast.Extent.StartLineNumber
+                    $Resolve.ScriptBlock.File
                 )
 
                 if( -not $PassThru ) {
                     $msg | New-Text -fg 'gray80' -bg 'gray30' | Write-Host
                 }
 
+                # throw 'nyi'
+
                 if( $AsCommand ) { return $Resolve }
-                return Get-Item $resolve.ScriptBlock.File
+                return Get-Item $Resolve.ScriptBlock.File
             }
             if( $Obj -is [Management.Automation.FunctionInfo] )  {
-                $Resolve = $Obj
+                [Management.Automation.FunctionInfo] $Resolve = $Obj
+
                 $msg = '    => start: {0} of "{1}"' -f @(
-                    $resolve.ScriptBlock.Ast.Extent.StartLineNumber
-                    $resolve.ScriptBlock.File
+                    $Resolve.ScriptBlock.Ast.Extent.StartLineNumber
+                    $Resolve.ScriptBlock.File
                 )
                 if( -not $PassThru ) {
                     $msg | New-Text -fg 'gray80' -bg 'gray30' | Write-Host
                 }
-
                 if( $AsCommand ) { return $Resolve }
-                return Get-Item $resolve.ScriptBlock.File
+
+                $file = Get-Item $resolve.ScriptBlock.File
+                $info = [ordered]@{
+                    PSTypeName        = 'mintils.FunctionInfo.withExtent'
+                    FileName          = $file.Name
+                    Path              = $File
+                    FullName          = $File.FullName
+                    StartLineNumber   = $Resolve.ScriptBlock.Ast.Extent.StartLineNumber
+                    EndLineNumber     = $Resolve.ScriptBlock.Ast.Extent.EndLineNumber
+                    StartColumnNumber = $Resolve.ScriptBlock.Ast.Extent.StartColumnNumber
+                    EndColumnNumber   = $Resolve.ScriptBlock.Ast.Extent.EndColumnNumber
+                    FromObject        = $Resolve
+                }
+                return [pscustomobject] $Info
             }
-            'Unhandled converting command path from type: {0}' -f $Obj.GetType().Name  | Write-Warning
+            'Unhandled converting command path from type: {0}' -f $Obj.GetType().Name
+                | Write-Warning
             return $Null
         }
         $binCode = Mint.Require-AppInfo -Name 'code'
     }
-
     process {
         $query = CoerceCommand -Obj $InputObject
         foreach($Item in $query) {
             if( $PassThru ) { $item; continue; }
+            if( -not (Test-Path $Item.FullName ) ) {
+                $msg = '.FullName not found on Item: {0}' -f $Item
+                $msg | Write-Warning
+                $msg | write-error
+                continue
+            }
             $binArgs = @(
                 '--goto'
                 ( Get-Item -ea 'stop' $item.FullName )
             )
+
+            if( $item.StartLineNumber -and $item.Path ) {
+                $binArgs = @(
+                    '--goto'
+                    '{0}:{1}' -f @(
+                        Get-Item -ea 'stop' $Item.Path.FullName
+                        $item.StartLineNumber
+                    )
+                )
+            }
+            if( -not $PassThru ) {
+                $binArgs
+                    | Join-String -sep ' ' -op '    invoke code => '
+                    | Write-Host -fg 'gray80' -bg 'gray30'
+            }
+
             & $binCode @binArgs
         }
 
