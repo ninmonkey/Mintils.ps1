c98fd3d75455e814db700378d59a172609de806b
Jake Bolton <ninmonkeys@gmail.com>
2025-12-27 12:36:04 -0600

    clean: `Mint.Find-FunctionDefinition` uses internal `_Resolve-CommandFileLocation`

--- Commands/Public/Find-MintilsFunctionDefinition.ps1
+++ Commands/Public/Find-MintilsFunctionDefinition.ps1
@@ -33,109 +33,60 @@
         [switch] $AsCommand
     )
     begin {
-        function CoerceCommand {
-            # Get filepath from command, scriptblock, alias, etc.
-            param(
-                [Parameter(Mandatory)] $Obj
-            )
-            if( $Obj -is [Management.Automation.AliasInfo] )  {
-                [Management.Automation.AliasInfo] $Cur = $Obj
-
-                if( $Cur.ResolvedCommand ) {
-                    $Cur.ResolvedCommand.Name
-                        '    => resolved: "{0}" -> {1}' -f @(
-                            $obj.Name,
-                            $Obj.ResolvedCommandName
-                        )
-                        | Write-Verbose
-                    return ( CoerceCommand -Obj $Cur.ResolvedCommand )
-                } else {
-                    '    => "{0}.ResolvedCommand" was falsy ' -f $Obj.Name
-                        | Write-Verbose
-                }
-                # CoerceCommand -Obj $Obj.ReferencedCommand
-
-                # $Resolve = Get-Command $Cur.Definition
-                # [System.Management.Automation.CommandInfo] $
-
-                $msg = '    => start: {0} of "{1}"' -f @(
-                    $Resolve.ScriptBlock.Ast.Extent.StartLineNumber
-                    $Resolve.ScriptBlock.File
-                )
-
-                if( -not $PassThru ) {
-                    $msg | New-Text -fg 'gray80' -bg 'gray30' | Write-Host
-                }
-
-                # throw 'nyi'
-
-                if( $AsCommand ) { return $Resolve }
-                return Get-Item $Resolve.ScriptBlock.File
-            }
-            if( $Obj -is [Management.Automation.FunctionInfo] )  {
-                [Management.Automation.FunctionInfo] $Resolve = $Obj
-
-                $msg = '    => start: {0} of "{1}"' -f @(
-                    $Resolve.ScriptBlock.Ast.Extent.StartLineNumber
-                    $Resolve.ScriptBlock.File
-                )
-                if( -not $PassThru ) {
-                    $msg | New-Text -fg 'gray80' -bg 'gray30' | Write-Host
-                }
-                if( $AsCommand ) { return $Resolve }
-
-                $file = Get-Item $resolve.ScriptBlock.File
-                $info = [ordered]@{
-                    PSTypeName        = 'mintils.FunctionInfo.withExtent'
-                    FileName          = $file.Name
-                    Path              = $File
-                    FullName          = $File.FullName
-                    StartLineNumber   = $Resolve.ScriptBlock.Ast.Extent.StartLineNumber
-                    EndLineNumber     = $Resolve.ScriptBlock.Ast.Extent.EndLineNumber
-                    StartColumnNumber = $Resolve.ScriptBlock.Ast.Extent.StartColumnNumber
-                    EndColumnNumber   = $Resolve.ScriptBlock.Ast.Extent.EndColumnNumber
-                    FromObject        = $Resolve
-                }
-                return [pscustomobject] $Info
-            }
-            'Unhandled converting command path from type: {0}' -f $Obj.GetType().Name
-                | Write-Warning
-            return $Null
-        }
         $binCode = Mint.Require-AppInfo -Name 'code'
+        write-warning 'WIP: New Find-Func implementation'
     }
     process {
-        $query = CoerceCommand -Obj $InputObject
-        foreach($Item in $query) {
-            if( $PassThru ) { $item; continue; }
-            if( -not (Test-Path $Item.FullName ) ) {
-                $msg = '.FullName not found on Item: {0}' -f $Item
-                $msg | Write-Warning
-                $msg | write-error
-                continue
-            }
-            $binArgs = @(
-                '--goto'
-                ( Get-Item -ea 'stop' $item.FullName )
-            )
+        $found = _ResolveCommandFileLocation -InputObject $InputObject -Verbose
+        if( $PassThru ) { $found ; return ; }
 
-            if( $item.StartLineNumber -and $item.Path ) {
-                $binArgs = @(
-                    '--goto'
-                    '{0}:{1}' -f @(
-                        Get-Item -ea 'stop' $Item.Path.FullName
-                        $item.StartLineNumber
-                    )
-                )
-            }
-            if( -not $PassThru ) {
-                $binArgs
-                    | Join-String -sep ' ' -op '    invoke code => '
-                    | Write-Host -fg 'gray80' -bg 'gray30'
-            }
+        $binArgs = @(
+            '--goto', $Found.FileWithLineNumberString
+        )
 
-            & $binCode @binArgs
+        if( -not $PassThru ) {
+            $binArgs
+                | Join-String -sep ' ' -op '    invoke code => '
+                | Write-Host -fg 'gray80' -bg 'gray30'
         }
+
+        if( -not (Test-Path $found.File )) {
+            throw "Filepath not found for: $( $InputObject.GetType() )"
+        }
+
+        & $binCode @( '--goto', $Found.FileWithLineNumberString )
+        # throw "old logic starts here"
+        # $query = _Resolve-CommandFileLocation -InputObject $InputObject
+        # foreach($Item in $query) {
+        #     if( $PassThru ) { $item; continue; }
+        #     if( -not (Test-Path $Item.FullName ) ) {
+        #         $msg = '.FullName not found on Item: {0}' -f $Item
+        #         $msg | Write-Warning
+        #         $msg | write-error
+        #         continue
+        #     }
+        #     $binArgs = @(
+        #         '--goto'
+        #         ( Get-Item -ea 'stop' $item.FullName )
+        #     )
+
+        #     if( $item.StartLineNumber -and $item.Path ) {
+        #         $binArgs = @(
+        #             '--goto'
+        #             '{0}:{1}' -f @(
+        #                 Get-Item -ea 'stop' $Item.Path.FullName
+        #                 $item.StartLineNumber
+        #             )
+        #         )
+        #     }
+        #     if( -not $PassThru ) {
+        #         $binArgs
+        #             | Join-String -sep ' ' -op '    invoke code => '
+        #             | Write-Host -fg 'gray80' -bg 'gray30'
+        #     }
+
+        #     & $binCode @binArgs
+        # }
 
     }
 }
