4f030d16cc8bc042996829f0aae495c70517fccc
Jake Bolton <ninmonkeys@gmail.com>
2026-01-04 15:16:38 -0600

    command `Mint.Convert.Dict.FromKeyValues` Fixes #127

---
+++ Commands/Private/_Coerce.Hashtable.FromKeyValuePairs.ps1
@@ -0,0 +1,44 @@
+﻿function _Coerce.HashTable.FromKeyValuePairs { # or FromDictionaryEntry
+    <#
+    .synopsis
+        [internal] Converts arrays of [Collections.DictionaryEntry] to one hashtable
+    .description
+        Does not sort keys, Does not return [ordered]@{}
+    .example
+        > $hash = Gci Env:\ | _Coerce.HashTable.FromDictionaryEntry
+        > Mint.Write-Dict $Hash
+    .example
+        > Mint.Write-Dict ( _Coerce.HashTable.FromDictionaryEntry -InputObject $sample )
+    .LINK
+        Mintils\Mint.ConvertTo-Hashtable.FromKeyValues
+    #>
+    [OutputType( [System.Collections.Hashtable] )] # [System.Collections.Specialized.OrderedDictionary],
+    [CmdletBinding()]
+    param(
+        [Parameter( Mandatory, ValueFromPipeline )]
+        [object[]] $InputObject
+    )
+    begin {
+        $hash = @{}
+    }
+    process {
+        foreach( $Item in $InputObject ) {
+            if( $Item -is [Collections.DictionaryEntry] ) {
+                [Collections.DictionaryEntry] $Obj = $Item
+                $hash[ $Obj.Key ] = $Obj.Value
+                continue
+            }
+            if( $null -ne $Item.Key -and $null -ne $Item.Value ) {
+                $hash[ $Item.Key  ] = $Item.Value
+                continue
+            }
+            "Unhandled Item type: {0}" -f @(
+                ( $Item )?.GetType() ?? '<null>'
+            )  | Write-Error
+            continue
+        }
+    }
+    end {
+        $hash
+    }
+}
---
+++ Commands/Public/Convert/ConvertTo-Hashtable.FromKeyValues.ps1
@@ -0,0 +1,42 @@
+﻿function Convert-MintilsHashtableFromKeyValues {
+    <#
+    .synopsis
+        [internal] Converts arrays of [Collections.DictionaryEntry] to one hashtable
+    .description
+        Does not sort keys, Does not return [ordered]@{}
+    .example
+        > Mint.Write-Dict ( Gci Env:\ | Mint.Convert.Dict.FromKeyValues )
+    .example
+        > $hash = Mint.Convert.Dict.FromKeyValues -InputObject ( Gci Env:\ )
+        > Mint.Write-Dict $hash
+    .example
+        > Mint.Write-Dict ( Mint.Convert.Dict.FromKeyValues -InputObject ( Gci Env:\ ) )
+    .example
+        > Mint.Write-Dict ( Mint.ConvertTo_Coerce.HashTable.FromDictionaryEntry -InputObject $sample )
+    #>
+    [Alias(
+        'Mint.ConvertTo-Hashtable.FromKeyValues',
+        'Mint.Convert-Dict.FromKeyValues',
+        # 'Mint.ConvertTo-Dict.FromKeyValues',
+        'Mint.Convert.Dict.FromKeyValues'
+        # 'Mint.Convert.Dict.FromPairs'
+        # Mint.Convert-Hash.FromDictEntry
+    )]
+    [OutputType( [System.Collections.Hashtable] )]
+    [CmdletBinding()]
+    param(
+        # hashtable or idictionary or what is best to iterate ?
+        [Alias('Pairs', 'KeyValues', 'DictionaryEntry')]
+        [Parameter( Mandatory, ValueFromPipeline )]
+        [object[]] $InputObject
+    )
+    begin {
+        [Collections.Generic.List[Object]] $List = @()
+    }
+    process {
+        $List.AddRange(@( $InputObject ))
+    }
+    end {
+        _Coerce.HashTable.FromKeyValuePairs -InputObject $List
+    }
+}
