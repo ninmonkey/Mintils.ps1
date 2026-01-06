e2e432c7b2b80de4fdf52e6952082a8bb8755021
Jake Bolton <ninmonkeys@gmail.com>
2026-01-06 13:17:56 -0600

    add: internal IsBlank commands
    pester: Add blank tests for: `_Is.Blank`, `_Is.Null`, `_Is.Empty`

---
+++ Commands/Private/Is/_Is.Blank.ps1
@@ -0,0 +1,62 @@
+﻿function _Is.Null {
+    <#
+    .synopsis
+        [internal] compare IsBlank, IsNull, IsEmpty, etc.
+    .DESCRIPTION
+        related: _Is.Null, _Is.Empty, _Is.Blank
+    #>
+    [OutputType( [bool] )]
+    [CmdletBinding()]
+    param(
+        [AllowNull()]
+        [AllowEmptyString()]
+        [AllowEmptyCollection()]
+        [Parameter( Mandatory, ValueFromPipeline )]
+        [object] $InputObject
+    )
+    process {
+        return $Null -eq $InputObject
+    }
+}
+
+function _Is.Empty {
+    <#
+    .synopsis
+        [internal] compare IsBlank, IsNull, IsEmpty, etc.
+    .DESCRIPTION
+        related: _Is.Null, _Is.Empty, _Is.Blank
+    #>
+    [OutputType( [bool] )]
+    [CmdletBinding()]
+    param(
+        [AllowNull()]
+        [AllowEmptyString()]
+        [AllowEmptyCollection()]
+        [Parameter( Mandatory, ValueFromPipeline )]
+        [object] $InputObject
+    )
+    process {
+        return [string]::IsNullOrEmpty( $InputObject )
+    }
+}
+
+function _Is.Blank {
+    <#
+    .synopsis
+        [internal] compare IsBlank, IsNull, IsEmpty, etc.
+    .DESCRIPTION
+        related: _Is.Null, _Is.Empty, _Is.Blank
+    #>
+    [OutputType( [bool] )]
+    [CmdletBinding()]
+    param(
+        [AllowNull()]
+        [AllowEmptyString()]
+        [AllowEmptyCollection()]
+        [Parameter( Mandatory, ValueFromPipeline )]
+        [object] $InputObject
+    )
+    process {
+        return [string]::IsNullOrWhiteSpace( $InputObject )
+    }
+}
---
+++ Tests/Private/_Is.BlankOrNull.tests.ps1
@@ -0,0 +1,84 @@
+﻿BeforeAll {
+    $error.clear()
+    $PSStyle.OutputRendering = 'ansi'
+
+    $Module = Join-path $PSScriptRoot '../../Mintils.psd1'
+    @(
+        Import-Module $Module -Force -PassThru
+    )
+        | Join-String -p { $_.Name, $_.Version -join ': ' } -op "importing:...`n" -f "`n - {0}"
+        | Pansies\Write-Host -fore 'goldenrod'
+}
+
+# This is verbose, but the edge cases should be defined
+Describe "Test: IsBlank" -tag 'internal' {
+    Context 'Is.Null' {
+        It 'from Param: <value> is <expected>' -ForEach @(
+            @{ Value = $null ; Expected = $true }
+            @{ Value = ''    ; Expected = $false }
+            @{ Value = ' '   ; Expected = $false }
+        ) {
+            InModuleScope 'Mintils' -Parameters $_ {
+                _Is.Null $Value   | Should -BeExactly $Expected
+            }
+        }
+        It 'from Pipeline: <value> is <expected>' -ForEach @(
+            @{ Value = $null ; Expected = $true }
+            @{ Value = ''    ; Expected = $false }
+            @{ Value = ' '   ; Expected = $false }
+        ) {
+            InModuleScope 'Mintils' -Parameters $_ {
+                $value | _Is.Null | Should -BeExactly $Expected
+            }
+        }
+    }
+    Context 'Is.Blank' {
+        It 'from Param: <value> is <expected>' -ForEach @(
+            @{ Value = $null   ; Expected = $true }
+            @{ Value = ''      ; Expected = $true }
+            @{ Value = ' '     ; Expected = $true }
+            @{ Value = ' foo'  ; Expected = $false }
+        ) {
+            InModuleScope 'Mintils' -Parameters $_  -ea continue {
+                # $ErrorActionPreference = 'break'
+                _Is.Blank $Value   | Should -BeExactly $Expected -ea continue
+            }
+        }
+        It 'from Pipeline: <value> is <expected>' -ForEach @(
+            @{ Value = $null   ; Expected = $true }
+            @{ Value = ''      ; Expected = $true }
+            @{ Value = ' '     ; Expected = $true }
+            @{ Value = ' foo'  ; Expected = $false }
+        ) {
+            InModuleScope 'Mintils' -Parameters $_ {
+                $value | _Is.Blank | Should -BeExactly $Expected
+            }
+        }
+    }
+    Context 'Is.Empty' {
+        It 'from Param: <value> is <expected>' -ForEach @(
+            @{ Value = $null   ; Expected = $true }
+            @{ Value = ''      ; Expected = $true }
+            @{ Value = ' '     ; Expected = $false }
+            @{ Value = ' foo'  ; Expected = $false }
+            @{ Value = @()     ; Expected = $true }
+        ) {
+            InModuleScope 'Mintils' -Parameters $_  -ea continue {
+                # $ErrorActionPreference = 'break'
+                _Is.Empty $Value   | Should -BeExactly $Expected -ea continue
+            }
+        }
+        It 'from Pipeline: <value> is <expected>' -ForEach @(
+            @{ Value = $null   ; Expected = $true }
+            @{ Value = ''      ; Expected = $true }
+            @{ Value = ' '     ; Expected = $false }
+            @{ Value = ' foo'  ; Expected = $false }
+            # @{ Value = ,@()     ; Expected = $true }
+        ) {
+            InModuleScope 'Mintils' -Parameters $_ {
+                # $ErrorActionPreference = 'break'
+                $value | _Is.Empty | Should -BeExactly $Expected
+            }
+        }
+    }
+}
